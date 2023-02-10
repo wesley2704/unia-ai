a4d4f1533300e3b0a6ba0a5d4bd5381f630329b0
Breno Juan <breno.juan06@gmail.com>
2023-02-10 14:50:12 -0300

    Adicionado pesos as tijelas

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 let tijelas = [{
     id:1,
     nome:"tijela pequena",
-     preco:9.99},
+     preco:9.99,
+     peso: 150,},
      {
         id:2,
         nome:"tijela média",
-         preco:14.99},
+         preco:14.99,
+          peso: 250},
          {
             id:3,
             nome:"tijela grande",
-             preco:19.99},
+             preco:19.99,
+               peso: 500},
              {
                 id:4,
                 nome:"tijela extra grande",
-                 preco:29.99},
-    ] 
+                 preco:29.99,
+                  peso: 1000},
+    ] ;
 let sacola = []
 
 function criarPedido(tijela,adicionais,){
@@ -29,4 +33,5 @@ function adicionarPedido(tijela,adicionais){
   let novoPedido = criarPedido(tijela,adicionais)
   sacola.push(novoPedido)
 }
+
 
