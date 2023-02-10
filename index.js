4c83f48569848654e0c0dd5ce77d5e52c1de5583
CARLOS EDUARDO XAVIER BARBOSA <125261@pmenos.com.br>
2023-02-10 14:47:47 -0300

    adc imagens

--- index.js
+++ index.js
@@ -1,32 +1,23 @@
-let tijelas = [{
-    id:1,
-    nome:"tijela pequena",
-     preco:9.99},
-     {
-        id:2,
+let tijelas = [
+     {id:1,
+        nome:"tijela pequena",
+        preco:9.99,
+        img:"https://cdn.discordapp.com/attachments/1073655592876527627/1073658482676482088/sorvete-brasileiro-de-acai-congelado-em-copo-plastico-com-morango-isolado-no-fundo-branco-vista-frontal-do-menu-de-verao_317399-989.png"},
+
+     {id:2,
         nome:"tijela média",
-         preco:14.99},
-         {
-            id:3,
-            nome:"tijela grande",
-             preco:19.99},
-             {
-                id:4,
-                nome:"tijela extra grande",
-                 preco:29.99},
+        preco:14.99,
+        img:"https://cdn.discordapp.com/attachments/1073655592876527627/1073655625898279002/e3354c05a75f8cd0a76f6a97768d288f.png"},
+
+     {id:3,
+        nome:"tijela grande",
+        preco:19.99,
+        img:"https://img.freepik.com/fotos-premium/acai-congelado-brasileiro-com-morango-e-banana-em-uma-tigela-branca-isolada-no-fundo-branco_317399-731.jpg?w=2000"},
+
+     {id:4,
+        nome:"tijela extra grande",
+         preco:29.99,
+         img:"https://cdn.discordapp.com/attachments/1073655592876527627/1073660205499424869/image-removebg-preview.png"}
+               
     ] 
-let sacola = []
-
-function criarPedido(tijela,adicionais,){
-  let pedido = {
-    tijela,
-    adicionais
-  }
-  return pedido
-}
-
-function adicionarPedido(tijela,adicionais){
-  let novoPedido = criarPedido(tijela,adicionais)
-  sacola.push(novoPedido)
-}
 
